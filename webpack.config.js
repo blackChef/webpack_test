215c92126f2a1de2a14d28bf1d591f0af4550f53
Esben Petersen <esbenspetersen@gmail.com>
2017-07-27 20:11:22 +0200

    add optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: { index: './index.js' },
-  output: { filename: './[name].build.js' }
+  output: { filename: './[name].build.js' },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ]
 };
